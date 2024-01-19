c6db2ff6740b0230e6fcfdc8789487a92a522fb4
Eduardo Tavares <94573220+EduardoTS1808@users.noreply.github.com>
2024-01-19 06:21:06 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const listaPresentes = [
     {id:44, title:'Kit Tapetes para Cozinha',    src:'./imagens/kit-tapete-cozinha-2.jpg', disponivel: false,},
     {id:45, title:'Lençol Elástico - Queen',    src:'./imagens/lencol-elastico.jpg', disponivel: true,},
     
-    {id:47, title:'Mini-processador de Alimentos',    src:'./imagens/mini-processador.jpg', disponivel: true,},
+    {id:47, title:'Mini-processador de Alimentos',    src:'./imagens/mini-processador.jpg', disponivel: false,},
     {id:52, title:'Jogo Panelas de Alumínio',    src:'./imagens/jogo-panelas-aluninio.jpg', disponivel: true,}, {id:54, title:'Churrasqueira Móvel',    src:'./imagens/churrasqueira.jpg', disponivel: true,},
     
 ]
